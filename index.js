b59f3010c76a34a7201ca4ccd7ff450a1870e4b0
BIKTOPBB <BIKTOPBB@gmail.com>
2018-07-27 13:34:10 +0300

    Конвертер валют с запросом курса на сайте Привата

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var Converter = require('./module/module.js');
+
+var conv = new Converter();
+
+conv.UAH_USD(100);
+conv.USD_UAH(100);
+conv.UAH_EUR(100);
+conv.EUR_UAH(100);
+conv.UAH_RUS(100);
+conv.RUS_UAH(100);
