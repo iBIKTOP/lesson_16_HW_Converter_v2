85d53a84b42486cb6bfbb639093fc043c4a96376
BIKTOPBB <BIKTOPBB@gmail.com>
2018-07-27 15:12:53 +0300

    Конвертер валют с запросом в банк Приват, вторая версия

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-
+var request = require('request');
 var Converter = require('./module/module.js');
 
-var conv = new Converter();
+request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var obj = JSON.parse(body);
 
-conv.UAH_USD(100);
-conv.USD_UAH(100);
-conv.UAH_EUR(100);
-conv.EUR_UAH(100);
-conv.UAH_RUS(100);
-conv.RUS_UAH(100);
+        var val = function (val1,val2) {
+            var baseCurrency = 0;
+            if (val1 === "UAH" && val2 === "USD") {
+                baseCurrency = Number(obj[0].sale);
+            }
+            else if (val1 === "USD" && val2 === "UAH") {
+                baseCurrency = Number(obj[0].buy);
+            }
+            else if (val1 === "UAH" && val2 === "EUR") {
+                baseCurrency = Number(obj[1].sale);
+            }
+            else if (val1 === "EUR" && val2 === "UAH") {
+                baseCurrency = Number(obj[1].buy);
+            }
+            else if (val1 === "UAH" && val2 === "RUS") {
+                baseCurrency = Number(obj[2].sale);
+            }
+            else if (val1 === "RUS" && val2 === "UAH") {
+                baseCurrency = Number(obj[2].buy);
+            }
+            return baseCurrency;
+        };
+        var conv = new Converter();
+        conv.UAH_USD(100, val("UAH","USD"));
+        conv.USD_UAH(100, val("USD","UAH"));
+        conv.UAH_EUR(100, val("UAH","EUR"));
+        conv.EUR_UAH(100, val("EUR","UAH"));
+        conv.UAH_RUS(100, val("UAH","RUS"));
+        conv.RUS_UAH(100, val("RUS","UAH"));
+
+        // console.log("UAH_USD: " + cash / baseCurrency[0].sale);
+        //данные для вывода можно посмотреть на сайте https://api.privatbank.ua/#p24/exchange
+    } else {
+        console.warn(error);
+    }
+});
+
+
+
+
+
--- module/module.js
+++ module/module.js
@@ -7,76 +7,34 @@ Converter.prototype.roundTwoDecimal = function(amound) {
 	return Math.round(amound *100)/100;
 };
 
-Converter.prototype.UAH_USD = function (cash) {
-    request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var baseCurrency = JSON.parse(body);
-            console.log("UAH_USD: " + cash / baseCurrency[0].sale);
-            //данные для вывода можно посмотреть на сайте https://api.privatbank.ua/#p24/exchange
-        } else {
-            console.warn(error);
-        }
-    });
+Converter.prototype.UAH_USD = function (cash, kurs) {
+    // return this.roundTwoDecimal(cash / kurs);
+    console.log("За " + cash + "грн сегодня можно преобрести " + this.roundTwoDecimal(cash / kurs) + "$");
 };
 
-Converter.prototype.USD_UAH = function (cash) {
-    request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var baseCurrency = JSON.parse(body);
-            console.log("USD_UAH: " + baseCurrency[0].buy * cash);
-            //данные для вывода можно посмотреть на сайте https://api.privatbank.ua/#p24/exchange
-        } else {
-            console.warn(error);
-        }
-    });
+Converter.prototype.USD_UAH = function (cash, kurs) {
+    // return this.roundTwoDecimal(cash * kurs);
+    console.log("За " + cash + "$ сегодня можно преобрести " + this.roundTwoDecimal(cash * kurs) + "грн");
 };
 
-Converter.prototype.UAH_EUR = function (cash) {
-    request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var baseCurrency = JSON.parse(body);
-            console.log("UAH_EUR: " + cash / baseCurrency[1].sale);
-            //данные для вывода можно посмотреть на сайте https://api.privatbank.ua/#p24/exchange
-        } else {
-            console.warn(error);
-        }
-    });
+Converter.prototype.UAH_EUR = function (cash, kurs) {
+    // return this.roundTwoDecimal(cash / kurs);
+    console.log("За " + cash + "грн сегодня можно преобрести " + this.roundTwoDecimal(cash / kurs) + "евро");
 };
 
-Converter.prototype.EUR_UAH = function (cash) {
-    request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var baseCurrency = JSON.parse(body);
-            console.log("EUR_UAH: " + cash * baseCurrency[1].buy);
-            //данные для вывода можно посмотреть на сайте https://api.privatbank.ua/#p24/exchange
-        } else {
-            console.warn(error);
-        }
-    });
+Converter.prototype.EUR_UAH = function (cash, kurs) {
+    // return this.roundTwoDecimal(cash * kurs);
+    console.log("За " + cash + "евро сегодня можно преобрести " + this.roundTwoDecimal(cash * kurs) + "грн");
 };
 
-Converter.prototype.UAH_RUS = function (cash) {
-    request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var baseCurrency = JSON.parse(body);
-            console.log("UAH_RUS: " + cash / baseCurrency[2].sale);
-            //данные для вывода можно посмотреть на сайте https://api.privatbank.ua/#p24/exchange
-        } else {
-            console.warn(error);
-        }
-    });
+Converter.prototype.UAH_RUS = function (cash, kurs) {
+    // return this.roundTwoDecimal(cash / kurs);
+    console.log("За " + cash + "грн сегодня можно преобрести " + this.roundTwoDecimal(cash / kurs) + "рублей");
 };
 
-Converter.prototype.RUS_UAH = function (cash) {
-    request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var baseCurrency = JSON.parse(body);
-            console.log("RUS_UAH: " + baseCurrency[2].buy * cash);
-            //данные для вывода можно посмотреть на сайте https://api.privatbank.ua/#p24/exchange
-        } else {
-            console.warn(error);
-        }
-    });
+Converter.prototype.RUS_UAH = function (cash, kurs) {
+    // return this.roundTwoDecimal(cash * kurs);
+    console.log("За " + cash + "рублей сегодня можно преобрести " + this.roundTwoDecimal(cash * kurs) + "грн");
 };
 
 module.exports = Converter;
